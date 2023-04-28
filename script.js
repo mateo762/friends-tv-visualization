6b70465ebe8b1039b4b772790be34a7cfdd98f93
Mateo Echeverry <mateo.echeverryhoyos@gmail.com>
2023-04-28 16:33:07 +0200

    Improve graph visualization

--- script.js
+++ script.js
@@ -2,39 +2,6 @@ const width = 800;
 const height = 600;
 
 d3.json("https://mateo762.github.io/friends_data/graph_data.json").then(function (graph) {
-    // Your d3.js code goes here
-    // You can access the nodes and links using graph.nodes and graph.links
-    // const nodes = [
-    //     { id: 'Ross', name: 'Ross Geller', group: 1 },
-    //     { id: 'Rachel', name: 'Rachel Green', group: 1 },
-    //     { id: 'Monica', name: 'Monica Geller', group: 1 },
-    //     { id: 'Chandler', name: 'Chandler Bing', group: 1 },
-    //     { id: 'Joey', name: 'Joey Tribbiani', group: 1 },
-    //     { id: 'Phoebe', name: 'Phoebe Buffay', group: 1 },
-    //     { id: 'Janice', name: 'Janice Litman', group: 2 },
-    //     { id: 'Gunther', name: 'Gunther', group: 2 }
-    // ];
-
-    // const edges = [ { source: 'Ross', target: 'Rachel', value: 150, sentiment: 'positive' },
-    //     { source: 'Ross', target: 'Monica', value: 120, sentiment: 'positive' },
-    //     { source: 'Ross', target: 'Chandler', value: 130, sentiment: 'positive' },
-    //     { source: 'Ross', target: 'Joey', value: 110, sentiment: 'positive' },
-    //     { source: 'Ross', target: 'Phoebe', value: 100, sentiment: 'positive' },
-    //     { source: 'Rachel', target: 'Monica', value: 140, sentiment: 'positive' },
-    //     { source: 'Rachel', target: 'Chandler', value: 90, sentiment: 'neutral' },
-    //     { source: 'Rachel', target: 'Joey', value: 95, sentiment: 'neutral' },
-    //     { source: 'Rachel', target: 'Phoebe', value: 105, sentiment: 'positive' },
-    //     { source: 'Monica', target: 'Chandler', value: 200, sentiment: 'positive' },
-    //     { source: 'Monica', target: 'Joey', value: 85, sentiment: 'neutral' },
-    //     { source: 'Monica', target: 'Phoebe', value: 115, sentiment: 'positive' },
-    //     { source: 'Chandler', target: 'Joey', value: 180, sentiment: 'positive' },
-    //     { source: 'Chandler', target: 'Phoebe', value: 80, sentiment: 'neutral' },
-    //     { source: 'Joey', target: 'Phoebe', value: 125, sentiment: 'positive' },
-    //     { source: 'Chandler', target: 'Janice', value: 60, sentiment: 'negative' },
-    //     { source: 'Ross', target: 'Janice', value: 20, sentiment: 'negative' },
-    //     { source: 'Rachel', target: 'Gunther', value: 30, sentiment: 'neutral' },
-    //     { source: 'Gunther', target: 'Ross', value: 25, sentiment: 'negative' }
-    // ];
 
     const nodes = graph.nodes
     const edges = graph.links
@@ -50,18 +17,45 @@ d3.json("https://mateo762.github.io/friends_data/graph_data.json").then(function
         .domain([minWeight, maxWeight])
         .range([1, 10]); // Adjust the range to suitable values for the edge thickness
 
-    const linkColor = d3.scaleOrdinal()
-        .domain(['positive', 'neutral', 'negative'])
-        .range(['#2ECC40', '#F1C40F', '#E74C3C']);
+    // Modify the linkColor scale
+    const linkColor = d3.scaleSequential(t => d3.interpolatePurples(0.3 + 0.7 * t)).domain([minWeight, maxWeight]);
+
 
     const simulation = d3.forceSimulation(nodes)
         .force("link", d3.forceLink(edges).id(d => d.id).distance(50))
-        .force("charge", d3.forceManyBody().strength(-200))
+        .force("charge", d3.forceManyBody().strength(-1200))
         .force("center", d3.forceCenter(width / 2, height / 2));
 
     const svg = d3.create("svg")
         .attr("viewBox", [0, 0, width, height]);
 
+
+    const edgeTooltip = svg.append("g")
+        .attr("class", "edge-tooltip")
+        .style("display", "none");
+
+    edgeTooltip.append("rect")
+        .attr("class", "edge-tooltip-rect")
+        .attr("width", 200)
+        .attr("height", 120)
+        .attr("fill", "#ccc")
+        .attr("stroke", "#000")
+        .attr("rx", 5)
+        .attr("ry", 5);
+
+    edgeTooltip.append("image")
+        .attr("x", 5)
+        .attr("y", 5)
+        .attr("width", 60)
+        .attr("height", 60)
+        .attr("class", "image-1");
+
+    edgeTooltip.append("image")
+        .attr("x", 135)
+        .attr("y", 5)
+        .attr("width", 60)
+        .attr("height", 60)
+        .attr("class", "image-2");
 
     // Create the tooltip-like element
     const tooltip = svg.append("g")
@@ -87,37 +81,126 @@ d3.json("https://mateo762.github.io/friends_data/graph_data.json").then(function
         .attr("y", 30)
         .attr("font-size", "14px");
 
+
+    svg.on("click", function (event) {
+        // Check if the event target is not a node
+        console.log(event.target.classList)
+        if (
+            !event.target.classList.contains("node") &&
+            !(event.target.parentElement && event.target.parentElement.classList.contains("node")) &&
+            !event.target.classList.contains("link") &&
+            !(event.target.parentElement && event.target.parentElement.classList.contains("link"))
+        ) {
+            // Hide the tooltip
+            tooltip.style("display", "none");
+            // Hide the edgeTooltip
+            edgeTooltip.style("display", "none");
+        }
+    });
+
     const link = svg.append("g")
         .attr("class", "links")
         .selectAll("line")
         .data(edges)
         .join("line")
-        .attr("stroke-width", d => edgeWeightScale(d.value))
-        .attr("stroke", d => linkColor(d.sentiment));
-
-    svg.on("click", function (event) {
-        // Check if the event target is not a node
-        if (!event.target.classList.contains("node")) {
-            // Hide the tooltip
+        .attr("stroke-width", d => 8)
+        .attr("class", "link")
+        .attr("stroke", d => linkColor(d.value))
+        .on("mouseover", function (event, d) {
+            // Change the stroke color to green on hover
+            d3.select(this).attr("stroke", "green");
+        })
+        .on("mouseout", function (event, d) {
+            // Reset the stroke color to its original value when the mouse leaves
+            d3.select(this).attr("stroke", linkColor(d.value));
+        })
+        .on("click", function (event, d) {
+            // Hide the character tooltip
             tooltip.style("display", "none");
-        }
-    });
+
+            // Get the mouse position
+            const [x, y] = d3.pointer(event);
+
+            // Display the edge tooltip
+            edgeTooltip.style("display", "block")
+
+            // Clear previous text elements
+            edgeTooltip.selectAll("text").remove();
+            console.log(d)
+
+            // Add a single text element for the phrases
+            const phraseText = edgeTooltip.append("text")
+                .attr("x", 100) // Half of the rectangle width
+                .attr("y", 100) // Half of the rectangle height
+                .attr("text-anchor", "middle") // Horizontally center the text
+                .attr("dominant-baseline", "middle") // Vertically center the text
+                .attr("font-size", "20px");
+
+            const imageName1 = d.source.id.split(' ')[0].toLowerCase();
+            const imageName2 = d.target.id.split(' ')[0].toLowerCase();
+            edgeTooltip.select('.image-1').attr("href", `pictures/${imageName1}.png`)
+            edgeTooltip.select('.image-2').attr("href", `pictures/${imageName2}.png`)
+
+
+            // Create a variable to keep track of the current phrase index
+            let phraseIndex = 0;
+
+            // Use d3.interval to update the displayed phrase every second
+            // Use d3.interval to update the displayed phrase every second
+            const interval = d3.interval(() => {
+                // Fade out the current phrase
+                phraseText.transition()
+                    .duration(500)
+                    .style("opacity", 0)
+                    .on("end", () => {
+                        // Increment the phrase index
+                        phraseIndex++;
+
+                        // If we've reached the end of the phrases, reset the index to 0
+                        if (phraseIndex >= d.phrases.length) {
+                            phraseIndex = 0;
+                        }
+
+                        // Update the text with the current phrase
+                        phraseText.text(d.phrases[phraseIndex].join(" "));
+
+                        // Fade in the updated phrase
+                        phraseText.transition()
+                            .duration(500)
+                            .style("opacity", 1);
+                    });
+            }, 1000);
+
+            // Stop the interval when the edge tooltip is hidden
+            edgeTooltip.on("mouseleave", () => {
+                interval.stop();
+            });
+        });
+
 
     // Update the node class
     const node = svg.append("g")
+        .attr('class', "g-nodes")
         .attr("class", "nodes")
         .selectAll("circle")
         .data(graph.nodes)
-        .join("circle")
+        .join("g") // Change this to 'g' instead of 'circle'
         .attr("class", "node") // Add this line
-        .attr("r", 5)
-        .attr("fill", d => color(d.group))
         .call(drag(simulation))
+        .on("mouseover", function (event, d) {
+            // Change the node color to green on hover
+            d3.select(this).select("circle").attr("fill", "green");
+        })
+        .on("mouseout", function (event, d) {
+            // Reset the node color to its original value when the mouse leaves
+            d3.select(this).select("circle").attr("fill", color(d.group));
+        })
         .on("click", function (event, d) {
             // Show the tooltip with character's name
             tooltip.style("display", "block");
             tooltip.select("text").text(d.id);
 
+            console.log(d)
             // Set the image source based on the character's name
             const imageName = d.id.split(' ')[0].toLowerCase();
 
@@ -126,6 +209,21 @@ d3.json("https://mateo762.github.io/friends_data/graph_data.json").then(function
             // Position the tooltip based on the node's coordinates
             tooltip.attr("transform", `translate(${d.x + 10}, ${d.y - 25})`);
         });
+
+    node.append("circle") // Add a circle element to the 'g' element
+        .attr("r", 20) // Change the radius to 30
+        .attr("fill", d => color(d.group));
+
+    node.append("image") // Add an image element to the 'g' element
+        .attr("x", -15) // Center the image horizontally
+        .attr("y", -15) // Center the image vertically
+        .attr("width", 30) // Set the image width
+        .attr("height", 30) // Set the image height
+        .attr("href", function (d) {
+            // Set the image source based on the character's name
+            const imageName = d.id.split(' ')[0].toLowerCase();
+            return `pictures/${imageName}.png`;
+        }); // Set the image source
 
     node.append("title")
         .text(d => d.name);
@@ -136,8 +234,7 @@ d3.json("https://mateo762.github.io/friends_data/graph_data.json").then(function
             .attr("x2", d => d.target.x)
             .attr("y2", d => d.target.y);
 
-        node.attr("cx", d => d.x)
-            .attr("cy", d => d.y);
+        node.attr("transform", d => `translate(${d.x}, ${d.y})`);
     });
 
     function drag(simulation) {
